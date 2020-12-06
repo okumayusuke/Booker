a6439127b0e607e86680bd42e141dad11b18302e
EC2 Default User <ec2-user@ip-172-31-82-159.ec2.internal>
2020-12-06 11:54:16 +0000

    [add]コントローラとモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'books/top'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/edit'
+
+  get 'books/n'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root to: 'books#top'
 end
