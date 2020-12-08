8ef46a7c78794c16bf2fae904bcdd3c4246c4044
EC2 Default User <ec2-user@ip-172-31-82-159.ec2.internal>
2020-12-08 09:42:27 +0000

    [update]機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/top'
-
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/edit'
-
-  get 'books/n'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   root to: 'books#top'
 end
